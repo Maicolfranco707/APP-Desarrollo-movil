70b62ace1b2894378906bda8b9f098e4d8c40346
Maicol Franco <maicolfrancolloreda478@gmail.com>
2026-08-27 20:51:25 -0500

    agrega filtro de niveles y pantalla de navegación

---
+++ src/navigation/ClassesStarck.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ClasesScreen from "../screens/ClasesScreen";
+import {color} from "../theme";
+
+const Stack = createNativeStackNavigator();
+
+export default function ClassesStarck() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen 
+            name="Home" 
+            component={ClasesScreen} 
+            options={{headerShown: false, }}
+            />
+            <Stack.Screen
+
+            />
+        </Stack.Navigator>
+    )
+     }
--- src/screens/ClasesScreen.js
+++ src/screens/ClasesScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, textInput, FlatList, ScrollView, StyleSheet, TextInput } from 'react-native';
 import Card from '../components/Card';
+import NivelFiltro from '../components/NivelFiltro';
 import {spacing, colors, typography} from '../theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import {Ionicons} from '@expo/vector-icons';
@@ -18,11 +19,33 @@ const ClasesScreen = ({navigation}) => {
           <Ionicons name="search" size={18} />
           <TextInput 
             placeholder="Buscar por nivel o profesor"
-            value={nivel}
-            onChangeText={setNivel}
+            value={busqueda}
+            onChangeText={setBusqueda}
             autoCorrect = {false}
+            autocomplete = {false}
           />
+          {busqueda.length > 0 && (
+            <Ionicons 
+            name="close-circle"
+             size={18} 
+             onPress={() => setBusqueda('')} 
+             />
+          )}
         </View>
+        <ScrollView
+        style={{flexgrow: 0}}
+        >
+          {
+            NIVELES.map((item) =>(
+              <NivelFiltro
+                etiqueta={item}
+                activo={nivel === item}
+                onPress={() => setNivel(item)}
+              />
+            ))
+          }
+
+        </ScrollView>
       </View>
     </View>
 
