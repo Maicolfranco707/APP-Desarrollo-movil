4b9558aca19c0e427b66a5e0e1f0aba5b9cbbef3
Maicol Franco <maicolfrancolloreda478@gmail.com>
2026-09-23 16:17:24 -0500

    se agregan useAlmacenamiento y ReservasContext

--- src/navigation/ClassesStarck.js
+++ src/navigation/ClassesStarck.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import ClasesScreen from "../screens/ClasesScreen";
-import {color} from "../theme";
+import { colors } from "../theme";
+import DetalleClaseScreen from "../screens/DetalleClaseScreen";
 
 const Stack = createNativeStackNavigator();
 
-export default function ClassesStarck() {
+export default function ClasesStack() {
+    const insets = useSafeAreaInsets();
+
     return (
-        <Stack.Navigator>
-            <Stack.Screen 
-                name="Home" 
-                component={ClasesScreen} 
-                options={{ headerShown: false }}
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    height: 60 + insets.top,
+                    backgroundColor: colors.superficie,
+                },
+                headerTitleStyle: {
+                    marginTop: insets.top / 2,
+                },
+            }}
+        >
+            <Stack.Screen
+                name="Home"
+                component={ClasesScreen}
             />
         </Stack.Navigator>
-    )
+    );
 }
